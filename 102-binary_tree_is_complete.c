68e3efba13f15af1abb0e6b0d2d6a3a7d39359cd
Angiecodes <angelcoder87@gmail.com>
2023-11-03 23:34:04 +0300

    Is complete

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,43 +1,49 @@
 #include "binary_trees.h"
+#include "queue.h"
 
 /**
- * binary_tree_is_complete - Check if a binary tree is complete.
- * @tree: Pointer to the root node of the tree to check.
- *
- * Return: 1 if the tree is complete, 0 otherwise.
+ * binary_tree_is_complete - checks if a binary tree is complete
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if the tree is complete, 0 otherwise
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	binary_tree_t *queue[1024];
-	int front = 0, rear = 0;
-	int null_flag = 0;
+	binary_tree_t *node;
+	queue_t *queue = NULL;
+	int non_complete = 0;
 
 	if (!tree)
-	return (0);
-
-	queue[rear] = (binary_tree_t *)tree;
-	rear++;
-
-	while (front < rear)
-	{
-
-	const binary_tree_t *current = queue[front];
-
-	front++;
-
-	if (!current)
-	{
-	null_flag = 1;
-	continue;
-	}
-
-	if (null_flag && current)
 		return (0);
 
-	queue[rear] = current->left;
-	rear++;
-	queue[rear] = current->right;
-	rear++;
+	queue = queue_push(queue, (void *)tree);
+
+	while (queue)
+	{
+	node = (binary_tree_t *)queue->data;
+	queue = dequeue(queue);
+
+	if (node->left)
+	{
+	if (non_complete)
+		return (0);
+	queue = queue_push(queue, (void *)node->left);
+	}
+	else
+	{
+	non_complete = 1;
+	}
+
+	if (node->right)
+	{
+	if (non_complete)
+		return (0);
+	queue = queue_push(queue, (void *)node->right);
+	}
+	else
+	{
+		non_complete = 1;
+	}
+	}
 
 	return (1);
 }
